b03718de4f3a3f9e2388659741bec2ec763b58ad
Mustafa Kan <mustafakan242@gmail.com>
2024-06-08 01:39:26 +0300

    app

---
+++ weatherapp/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+import {Info} from './components/Info';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Form></Form>
+      <Info></Info>
+    </div>
+  );
+}
+
+export default App;
